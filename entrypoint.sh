4bacf46b4a20d8d6687f835726e7d5050691d043
Johan Hansson <johan@anyfin.com>
2020-07-08 22:34:57 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
-echo ${INPUT_GOOGLE_APPLICATION_CREDENTIALS}
-echo  $INPUT_GOOGLE_APPLICATION_CREDENTIALS
+
 echo "${INPUT_GOOGLE_APPLICATION_CREDENTIALS}" | base64 -d > "${HOME}/gcloud.json" 
 gcloud auth activate-service-account --key-file="${HOME}/gcloud.json"
 
 # Build image
-gcloud builds submit --tag "$TEMPLATE_IMAGE" "$PATH_DOCKERFILE"
+gcloud builds submit --tag "$INPUT_IMAGE_TAG" "$INPUT_PATH_DOCKERFILE"
 
 # Build the Flex Template.
 gcloud beta dataflow flex-template build $INPUT_TEMPLATE_PATH \
