079e13d76664dd153345d597476ef3e7304985a3
Niklas Hansson <niklas.sven.hansson@gmail.com>
2020-07-09 23:23:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ gcloud auth activate-service-account --key-file="${HOME}/gcloud.json" --project 
 # Build image
 gcloud builds submit --tag "$INPUT_IMAGE_TAG" "$INPUT_DOCKERIMAGE_PATH"
 
-echo "$INPUT_TEMPLATE_IMAGE"
-echo "$INPUT_TEMPLATE_IMAGE"
-echo "$INPUT_TEMPLATE_IMAGE"
+echo "$INPUT_TEMPLATE_PATH"
+echo "$INPUT_TEMPLATE_PATH"
+echo "$INPUT_TEMPLATE_PATH"
 # Build the Flex Template.
 gcloud beta dataflow flex-template build $INPUT_TEMPLATE_PATH \
   --image "$INPUT_IMAGE_TAG" \
