7df9203f8d19450a5649029817ad3cf826b5323b
Niklas Hansson <niklas.sven.hansson@gmail.com>
2020-07-09 23:21:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ echo "$INPUT_TEMPLATE_IMAGE"
 echo "$INPUT_TEMPLATE_IMAGE"
 # Build the Flex Template.
 gcloud beta dataflow flex-template build $INPUT_TEMPLATE_PATH \
-  --image "$INPUT_TEMPLATE_IMAGE" \
+  --image "$INPUT_IMAGE_TAG" \
   --sdk-language "PYTHON" \
   --metadata-file "$INPUT_METADATA_PATH"
