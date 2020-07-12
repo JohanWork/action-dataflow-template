00de301db0453eb657315f048805a3bcbc23fce4
Niklas Hansson <niklas.sven.hansson@gmail.com>
2020-07-12 18:48:54 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 echo "${INPUT_GOOGLE_APPLICATION_CREDENTIALS}" | base64 -d > "${HOME}/gcloud.json" 
 gcloud auth activate-service-account --key-file="${HOME}/gcloud.json" --project "${INPUT_GCP_PROJECT}"
 
+echo $INPUT_IMAGE_TAG
+echo $INPUT_IMAGE_TAG
+echo $INPUT_DOCKERIMAGE_PATH
+echo $INPUT_DOCKERIMAGE_PATH
 # Build image
 gcloud builds submit --tag "$INPUT_IMAGE_TAG" "$INPUT_DOCKERIMAGE_PATH"
 
